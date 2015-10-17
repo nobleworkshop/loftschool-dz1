1383c2b1e36e998658f72ed6b9514360b0cb2372
Yurij Kharin <yurij.kharin@gmail.com>
2015-10-17 16:11:13 +0400

    login page, working on modal window - add new work

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,13 @@
+/* global require */
 var gulp = require("gulp"),
-	browserSync = require('browser-sync');
-
-var less = require('gulp-less');
-
-
-//LESS task
-gulp.task('less', function(){
-	gulp.src('app/less/main.less')
-	.pipe(less())
-	.pipe(gulp.dest('app/css/'))
-});
+	browserSync = require('browser-sync'),
+	less = require('gulp-less');
 
 
-gulp.watch('app/less/**/*.less', [less]);
-
-//Сервер
+/*******************************************
+ * APP
+ ******************************************/
+// Запускаем локальный сервер
 gulp.task('server', function (){
 	browserSync({
 		port: 9000,
@@ -24,15 +17,29 @@ gulp.task('server', function (){
 	});
 });
 
-// Слежка
+
+//LESS task для компиляции для компиляции LESS файлов
+gulp.task('less', function(){
+	gulp.src('app/less/main.less')
+	.pipe(less())
+	.pipe(gulp.dest('app/css/'))
+});
+
+
+gulp.watch('app/less/**/*.less', [less]);
+
+
+// Следим за файлами
 gulp.task('watch', function (){
 
+	// Слежка за обычными файлами и livereload
 	gulp.watch([
 		'app/*.html',
 		'app/js/**/*.js',
 		'app/css/**/*.css'
 	]).on('change', browserSync.reload);
 
+	// Слежка за LESS файлами и их компиляция
 	gulp.watch([
 		'app/less/**/*.less'
 	]).on('change', less);
@@ -40,7 +47,7 @@ gulp.task('watch', function (){
 });
 
 
-
+// Дефолтный таск для команды gulp по умолчанию
 gulp.task('default', ['server', 'watch', 'less']);
 
 
