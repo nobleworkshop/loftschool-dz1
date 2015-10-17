5b51a14ef6d60ac9695c55d8bf7bb51bfa2e33da
Yurij Kharin <yurij.kharin@gmail.com>
2015-10-17 17:02:14 +0400

    Finished - Modal on Works page
    Modal - to Add new work

---
+++ app/js/main.js
@@ -0,0 +1,26 @@
+
+
+// Semicolon (;) to ensure closing of earlier scripting
+// Encapsulation
+// $ is assigned to jQuery
+;
+(function($) {
+
+    // DOM Ready
+    $(function() {
+
+        // Binding a click event
+        // From jQuery v.1.7.0 use .on() instead of .bind()
+        $('#add_new_item').on('click', function(e) {
+
+            // Prevents the default action to be triggered. 
+            e.preventDefault();
+
+            // Triggering bPopup when click event is fired
+            $('#add_new_project_popup').bPopup();
+
+        });
+
+    });
+
+})(jQuery);
